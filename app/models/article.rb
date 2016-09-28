d827a3e79eabbcbbb4a38fde5bc94bdb810e0ed7
kopiusmaximus <alexander.kopynec@gmail.com>
2016-09-28 13:47:10 -0400

    Full association tests for model Article
    Full set of tests for associations for the Article model. Includes
    relationsip with Comments, which has yet to be defined.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,10 @@
 class Article < ActiveRecord::Base
+  validates :content, presence: true
+  # same as:
+  # validates_presence_of :content
+  # (but the latter is older and not preferred)
+
+  validates :title, presence: true
+
+  has_many :comments, inverse_of: :article, dependent: :destroy
 end
